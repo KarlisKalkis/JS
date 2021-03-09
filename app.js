ee76123a85d479c59dfcf8182851a6201563c9db
KarlisKalkis <kkalkis21@gmail.com>
2021-03-09 14:36:23 +0200

    3. stunda

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//console.log(Array.isArray(titles))
+//console.log(Array.isArray(Array.from(titles)))
+
+var titles = document.getElementsByClassName('titles')
+Array.from(titles).forEach(function(item){
+    console.log(item);
+})
+    
+
+
 const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
