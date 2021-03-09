a8bc5aab62669f129a5df879f05c5551880bed99
KarlisKalkis <kkalkis21@gmail.com>
2021-03-09 14:49:44 +0200

    4.stunda

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-//console.log(Array.isArray(titles))
-//console.log(Array.isArray(Array.from(titles)))
+const wmf = document.querySelector('#book-list li:nth-child(2).name');
+//console.log(wmf);
 
-var titles = document.getElementsByClassName('titles')
-Array.from(titles).forEach(function(item){
-    console.log(item);
+var books = document.querySelector('#book-list li .name')
+//console.log(books)
+
+books = document.querySelectorAll('#book-list li .name')
+//console.log(books)
+
+Array.from(books).forEach(function(book){
+    console.log(book)
 })
-    
-
-
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
-
-console.log(search, bookList);
