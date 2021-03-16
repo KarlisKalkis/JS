e81cd7973f18248c41a0ff47293471736e9eedfd
KarlisKalkis <kkalkis21@gmail.com>
2021-03-16 14:44:26 +0200

    10.stundas uzlabojumi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const list = document.querySelector('#book-list ul');
 
-list.addEventListener('click'.function(e),{
-    if(e  = 'delete'){
+list.addEventListener('click',function(e){
+    if(e.target.className  == 'delete'){
        const li = e.target.parentElement;
        list.removeChild(li)
     }
