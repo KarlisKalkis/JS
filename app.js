27df11acb6adda46f0ad9b015d83b793b27cb34e
KarlisKalkis <kkalkis21@gmail.com>
2021-03-16 15:18:56 +0200

    11.versija uzlabota

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const list = document.querySelector('#book-list ul');
 
+
+//delete books
 list.addEventListener('click',function(e){
     if(e.target.className  == 'delete'){
        const li = e.target.parentElement;
@@ -8,13 +10,13 @@ list.addEventListener('click',function(e){
 })
 
 
-//add book
+//add book.list
 
-const addForm = document.forms['add-book']
+const addForm = document.forms['add-book'];
 
 addForm.addEventListener('submit',function(e){
     e.preventDefault();
-    const value = addForm.querySelector('input(type="text")').value;
+    const value = addForm.querySelector('input[type="text"]').value;
     console.log(value);
 
 })
